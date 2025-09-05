b8a0faf0e430172a5714af4087be226ceaa0bf61
Priyanka Srivastava <priyankarenu20@gmail.com>
2025-09-05 16:18:30 +0530

    Fix port for Render deployment

--- server/index.ts
+++ server/index.ts
@@ -61,14 +61,14 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(
   {
     port,
-    host: "127.0.0.1",
+    host: "0.0.0.0",
   },
   () => {
-    log(`serving on http://127.0.0.1:${port}`);
+    log(`serving on http://0.0.0.0:${port}`);
   }
 );
 
